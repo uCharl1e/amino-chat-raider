6fcf6cc25e3f2f993264ff8bdb38d3d7069d055a
Charlie <81828220+uCharl1e@users.noreply.github.com>
2021-05-01 20:52:59 +0100

    Neatened it up a bit

--- script.py
+++ script.py
@@ -1,28 +1,29 @@
 import amino
 
 print("╔════════════════════════════════════════╗")
-print(" ---- Amino Chat Raider by uCharl1e ----")
+print("      Amino Chat Raider by uCharl1e")
 print("╚════════════════════════════════════════╝")
-print("-- login --")
+
+print("\n-- login --")
 email= input("Enter your email address >> ")
 password= input("Enter your password >> ")
 client=amino.Client()
 client.login(email=email,password=password)
 print("\nLogged in...")
-
-print("-- choose A victim --")
+print("-- choose A community --")
 comlist= client.sub_clients(size=100)
 for name, id in zip(comlist.name,comlist.comId):
    print("Comm name: ",name,"\nComm Id:",id,"\n-----------")
 ID= input("Please enter Community ID >> ")
+print("-- choose A chat --")
 subclient = amino.SubClient(comId=ID, profile=client.profile)
 chats = subclient.get_public_chat_threads(size=1000)
 for name, id in zip(chats.title, chats.chatId):
-    print(name,":", id)
+    print(name,":", id, "\n")
+print("-----------")
 Chat= input("Please paste chat ID here >> ")
 client.join_community(comId=ID)
 print("\nJoined Community...")
-
 print("-- start --")
 subclient.join_chat(chatId=Chat)
 print("\nAnd so... the chaos begins...")
